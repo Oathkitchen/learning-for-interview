99b70dfc9d85998c1a3eca0fe054742e3695ccc1
zhao.dp <zhao.dp@neusoft.com>
2025-02-08 13:04:39 +0800

    feat(react): add react playground

---
+++ react/my-app/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function Page() {
+  return (
+    <div className="App">
+      <input
+        onChange={(e) => {
+          console.log("Event: ", e);
+          console.log("NativeEvent: ", e.nativeEvent);
+          console.log("CurrentTarget: ", e.nativeEvent.currentTarget);
+          console.log("NativeEvent Type: ", e.nativeEvent.type);
+        }}
+      />
+    </div>
+  );
+}
