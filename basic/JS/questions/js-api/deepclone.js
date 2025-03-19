8feef5e6897a6df64763ea526ba557cce12be2aa
zhao.dp <zhao.dp@neusoft.com>
2025-03-19 21:18:24 +0800

    feat(js): 题

--- basic/JS/questions/js-api/deepclone.js
+++ basic/JS/questions/js-api/deepclone.js
@@ -1,74 +1,31 @@
- // JS 深克隆时如何处理循环引用
-//  function deepClone(obj, hash = new WeakMap()) {
-//   // 处理基本类型和 null/undefined
-//   if (obj === null || typeof obj !== "object") {
-//     return obj;
-//   }
+function deepClone(originalObj, hasMap = new WeakMap()) {
+  if(originalObj === null || typeof originalObj !== 'object') return originalObj
 
-//   // 处理循环引用：若已克隆过，直接返回克隆副本
-//   if (hash.has(obj)) {
-//     return hash.get(obj);
-//   }
+  if(hasMap.has(originalObj)) return hasMap.get(originalObj)
 
-//   // 处理特殊对象类型
-//   let clone;
-//   if (obj instanceof Date) {
-//     clone = new Date(obj);
-//   } else if (obj instanceof RegExp) {
-//     clone = new RegExp(obj);
-//   } else if (obj instanceof Map) {
-//     clone = new Map([...obj]);
-//   } else if (obj instanceof Set) {
-//     clone = new Set([...obj]);
-//   } else if (Array.isArray(obj)) {
-//     clone = [];
-//   } else {
-//     clone = Object.create(Object.getPrototypeOf(obj));
-//   }
+  let clone  = undefined
 
-//   // 记录当前对象和克隆对象的映射关系
-//   hash.set(obj, clone);
-
-//   // 递归克隆所有属性
-//   for (const key of Reflect.ownKeys(obj)) {
-//     // 包含 Symbol 属性
-//     const value = obj[key];
-//     clone[key] = deepClone(value, hash);
-//   }
-
-//   return clone;
-// }
-
-
-function deepClone(oriObj, hash = new WeakMap()){
-  if(oriObj === null || typeof oriObj !== 'object') return oriObj
-
-  if(hash.has(oriObj)) return hash.get(oriObj)
-
-  let clone = null
-  if(oriObj instanceof RegExp){
-    clone = new RegExp(oriObj)
-  } else if(oriObj instanceof Date){
-    clone = new Date(oriObj)
-  } else if(oriObj instanceof Map){
-    clone = new Map(...oriObj)
-  } else if(oriObj instanceof Set){
-    clone = new Set(...oriObj)
-  } else if(Array.isArray(oriObj)) {
+  if(originalObj instanceof RegExp){
+    clone = new RegExp(originalObj)
+  } else if(originalObj instanceof Date) {
+    clone = new Date(originalObj)
+  } else if(originalObj instanceof Map){
+    clone = new Map(...originalObj)
+  } else if (Array.isArray(originalObj)){
     clone = []
   } else {
-    clone = Object.create(Object.getPrototypeOf(oriObj))
+    clone = Object.create(Object.getPrototypeOf(originalObj))
   }
 
-  hash.set(oriObj, clone)
+  hasMap.set(originalObj, clone)
 
-  for(const key of Reflect.ownKeys(oriObj)) {
-    const value = oriObj[key]
-    clone[key] = deepClone(value, hash)
+  for (const key of Reflect.ownKeys(originalObj)){
+    const value = originalObj[key]
+    clone[key] = deepClone(value, hasMap)
   }
 
-  return clone 
-
+  return clone
+ 
 }
 
 export default deepClone
