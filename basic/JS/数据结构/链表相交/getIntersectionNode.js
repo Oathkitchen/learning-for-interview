b9253860031b2ef210b0ef0453151acd7d3a8aa5
zhao.dp <zhao.dp@neusoft.com>
2025-03-25 22:12:22 +0800

    feat(continue): contineu

--- basic/JS/数据结构/链表相交/getIntersectionNode.js
+++ basic/JS/数据结构/链表相交/getIntersectionNode.js
@@ -8,3 +8,13 @@ export function getIntersectionNode(listA, listB){
   return listA
 
 }
+
+// var getIntersectionNode = function(headA, headB) {
+//   let a = headA
+//   let b = headB
+//   while(a !== b){
+//       a = a.next ? a.next : headB
+//       b = b.next ? b.next : headA
+//   }
+//   return a
+// }
