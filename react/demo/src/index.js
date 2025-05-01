d2a8185b6d0f656f99fc787f12c67d379de9d48a
zhao.dp <zhao.dp@neusoft.com>
2025-05-01 13:37:15 +0800

    feat(react): demo setup

--- react/demo/src/index.js
+++ react/demo/src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/index.less";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>test</div>
   </React.StrictMode>
 );
+
+fetch("/jian/subscriptions/recommended_collections")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+  });
+
+fetch("/zhi/news/latest")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+  });
